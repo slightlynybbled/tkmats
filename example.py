2237f660c350d33ab63bb8aa4e645cf47908604f
Jason R. Jones <slightlynybbled@gmail.com>
2019-03-25 14:01:38 -0400

    Updated based on ate v0.18

--- example.py
+++ example.py
@@ -12,16 +12,15 @@ from tkate import TkAteFrame
 # be implemented.  Only the `execute()` method is implemented.
 class CommunicationTest(Test):
     def __init__(self, loglevel=logging.INFO):
-        super().__init__(moniker='communications test', loglevel=loglevel)
+        super().__init__(moniker='communications test',
+                         pass_if=True,
+                         loglevel=loglevel)
 
     # overriding the execute method
-    def execute(self, aborted=False, is_passing=False):
+    def execute(self, aborted, is_passing):
         # a normal test would set `test_is_passing` based on real conditions, we
         # are implementing a random value here simply for illustrative purposes
         passing = choice([True] * 3 + [False])
-
-        if not passing:
-            self.fail()
 
         # should return a (key, value) which are the results of the test
         return passing
@@ -30,31 +29,29 @@ class CommunicationTest(Test):
 # The PumpFlowTest implements the `setup' and `teardown` methods as well in order to demonstrate what that may look like
 class PumpFlowTest(Test):
     def __init__(self, loglevel=logging.INFO):
-        super().__init__(moniker='pump flow test', loglevel=loglevel)
+        super().__init__(moniker='pump flow test',
+                         min_value=5.6, max_value=6.4,
+                         loglevel=loglevel)
 
-    def setup(self, aborted=False, is_passing=False):
+    def setup(self, aborted, is_passing):
         # setting the speed of the pump might be something done in the setup, including
         # the wait time to speed up the pump, which we will simulate with a 2s sleep
         sleep(2.0)
 
-    def execute(self, aborted=False, is_passing=False):
+    def execute(self, aborted, is_passing):
         # user may abort the test based on the `aborted` or may
         # continue the test, at the author's discretion
-        if aborted or not is_passing:
+        if aborted:
             return None
 
         # simulate long-running process, such as several flow measurement/averaging cycles
         sleep(0.1)
         flow = 5.5 + random()
 
-        # apply conditions, fail the test if outside of those conditions
-        if not 5.6 <= flow <= 6.4:
-            self.fail()
-
         # should return a (key, value) tuple which are the results of the test
         return flow
 
-    def teardown(self, aborted=False, is_passing=False):
+    def teardown(self, aborted, is_passing):
         # again, simulating another long-running process...
         sleep(0.1)
 
