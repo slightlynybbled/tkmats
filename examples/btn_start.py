8ca76bbceb4db53f5e1d47ae7486ccae30d81d41
Jason R. Jones <slightlynybbled@gmail.com>
2019-04-10 22:43:02 -0400

    Re-organizing the examples

--- examples/btn_start.py
+++ examples/btn_start.py
@@ -8,8 +8,8 @@ from ate import TestSequence
 from tkate import TkAteFrame
 
 
-# The CommunicationTest class shows the minimum test structure that might be reasonably
-# be implemented.  Only the `execute()` method is implemented.
+# The CommunicationTest class shows the minimum test structure that might
+# be reasonably be implemented.  Only the `execute()` method is implemented.
 class CommunicationTest(Test):
     def __init__(self, loglevel=logging.INFO):
         super().__init__(moniker='communications test',
@@ -17,7 +17,7 @@ class CommunicationTest(Test):
                          loglevel=loglevel)
 
     # overriding the execute method
-    def execute(self, aborted, is_passing):
+    def execute(self, is_passing):
         # a normal test would set `test_is_passing` based on real conditions, we
         # are implementing a random value here simply for illustrative purposes
         passing = choice([True] * 3 + [False])
@@ -26,32 +26,30 @@ class CommunicationTest(Test):
         return passing
 
 
-# The PumpFlowTest implements the `setup' and `teardown` methods as well in order to demonstrate what that may look like
+# The PumpFlowTest implements the `setup' and `teardown` methods as well
+# in order to demonstrate what that may look like
 class PumpFlowTest(Test):
     def __init__(self, loglevel=logging.INFO):
         super().__init__(moniker='pump flow test',
                          min_value=5.6, max_value=6.4,
                          loglevel=loglevel)
 
-    def setup(self, aborted, is_passing):
-        # setting the speed of the pump might be something done in the setup, including
-        # the wait time to speed up the pump, which we will simulate with a 2s sleep
+    def setup(self, is_passing):
+        # setting the speed of the pump might be something done in the setup,
+        # including the wait time to speed up the pump, which we will
+        # simulate with a 2s sleep
         sleep(2.0)
 
-    def execute(self, aborted, is_passing):
-        # user may abort the test based on the `aborted` or may
-        # continue the test, at the author's discretion
-        if aborted:
-            return None
-
-        # simulate long-running process, such as several flow measurement/averaging cycles
+    def execute(self, is_passing):
+        # simulate long-running process, such as
+        # several flow measurement/averaging cycles
         sleep(0.1)
         flow = 5.5 + random()
 
         # should return a (key, value) tuple which are the results of the test
         return flow
 
-    def teardown(self, aborted, is_passing):
+    def teardown(self, is_passing):
         # again, simulating another long-running process...
         sleep(0.1)
 
